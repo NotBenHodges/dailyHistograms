79acb4911efba45f91d20c70a32b15ea00012e59
Ben Hodges <ben.hodges@centre.edu>
2019-03-26 15:04:51 -0400

    thing

--- stuff.js
+++ stuff.js
@@ -1,7 +1,8 @@
 var gradesP = d3.json("classData.json")
 
 gradesP.then(function(data){
-  console.log(data);
+  //console.log(data);
+  drawHistogram(data);
 });
 
 var drawHistogram = function(d){
@@ -10,10 +11,16 @@ var drawHistogram = function(d){
                 .domain(d3.extent(d))
                 .range([0,10]);
 
+  /* var yScale = d3.scaleLinear()
+                .domain([0,10])
+                .range([,0]); */
+
   var binMaker = d3.histogram()
                   .domain(xScale.domain())
                   .thresholds(xScale.ticks(5));
 
   var bins = binMaker(d);
+  console.log(bins);
+
 
 }
