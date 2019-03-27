d499fa70182065f4be6b3b9ebd6d513aba78fcd0
Xapakas <47224657+Xapakas@users.noreply.github.com>
2019-03-27 19:52:13 -0400

    actually a histogram this time

--- stuff.js
+++ stuff.js
@@ -17,9 +17,13 @@ var drawHistogram = function(d,daynumber){
 
   var binMaker = d3.histogram()
                   .domain(xScale.domain())
-                  .thresholds(xScale.ticks(5));
+                  .thresholds(xScale.ticks(4));
 
-  // var bins = binMaker(d.quizes[daynumber].grade);
+  var bins = binMaker(d.map(function(element){
+    return(element.quizes[19].grade);
+  }));
+
+  console.log(bins)
 
   // console.log(d[0].quizes[0].grade)
 
@@ -35,23 +39,39 @@ var drawHistogram = function(d,daynumber){
               .attr("width",width)
               .attr("height",height);
 
-  var barWidth = width / 23;
+  var barWidth = width / 6;
+
+  // svg.selectAll("rect")
+  //    .data(d)
+  //    .enter()
+  //    .append("rect")
+  //    .attr("x", function(d,i){
+  //      return i * barWidth;
+  //    })
+  //    .attr("y", function(d){
+  //      return height - (d.quizes[0].grade * (height/10));
+  //    })
+  //    .attr("height", (function(d){
+  //      return d.quizes[0].grade * (height/10);
+  //    }))
+  //    .attr("width", barWidth)
+  //    .attr("fill","blue");
 
   svg.selectAll("rect")
-     .data(d)
+     .data(bins)
      .enter()
      .append("rect")
-     .attr("x", function(d,i){
+     .attr("x",function(d,i){
        return i * barWidth;
      })
      .attr("y", function(d){
-       return height - (d.quizes[0].grade * (height/10));
+       return height - (d.length * (height/15));
      })
-     .attr("height", (function(d){
-       return d.quizes[0].grade * (height/10);
-     }))
-     .attr("width", barWidth)
-     .attr("fill","blue");
+     .attr("width",barWidth)
+     .attr("height",function(d){
+       return d.length * (height/15);
+     })
+     .attr("fill","green");
 
 
 }
